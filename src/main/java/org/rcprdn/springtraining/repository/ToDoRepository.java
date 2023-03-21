bb3e50b4db4630853ab76154b228b4ea640cb866
Rico Prodan <rico.prodan@gmail.com>
2023-03-21 13:10:21 +0100

    added repository and db connection

--- src/main/java/org/rcprdn/springtraining/repository/ToDoRepository.java
+++ src/main/java/org/rcprdn/springtraining/repository/ToDoRepository.java
@@ -4,9 +4,6 @@ import org.rcprdn.springtraining.entity.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.Serializable;
-import java.util.List;
-
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
 
