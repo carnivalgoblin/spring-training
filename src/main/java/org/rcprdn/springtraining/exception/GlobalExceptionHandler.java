0b7ca6e8216763a67eaedf19a71ae1d85f302bfe
Rico Prodan <rico.prodan@gmail.com>
2023-03-23 13:05:24 +0100

    added exception logging

--- src/main/java/org/rcprdn/springtraining/exception/GlobalExceptionHandler.java
+++ src/main/java/org/rcprdn/springtraining/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.rcprdn.springtraining.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +12,18 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
+@Log
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<Object> handleException(RuntimeException rex, Exception ex) {
+    logger.warn("INTERNAL SERVER ERROR");
     return new ResponseEntity<>(ErrorResponse.create(ex, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(EntityNotFoundException.class)
   protected ResponseEntity<Object> entityNotFoundException(RuntimeException rex, WebRequest request) {
+    logger.warn("WARNING! ENTITY NOT FOUND");
     return handleExceptionInternal(rex, rex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
   }
 
